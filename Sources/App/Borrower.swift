5a5047c3443718ae27d1dbb08408dddf7ae42a82
hajarAlshehri22 <147072959+hajarAlshehri22@users.noreply.github.com>
2024-02-15 17:39:16 +0300

    implement today exercise

---
+++ Sources/App/Borrower.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by Hajar Alshehri on 05/08/1445 AH.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
